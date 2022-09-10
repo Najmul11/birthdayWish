7bd4537cd6fead8e15288fb3cae6b2139ec156de
Najmul <najmulbinnurul@gmail.com>
2022-09-10 10:52:19 +0600

    music added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// countdown
 let days=document.getElementById('days');
 let hours=document.getElementById('hours');
 let minutes=document.getElementById('minutes');
@@ -30,5 +31,30 @@ let countdown=setInterval(() => {
     window.location.href='birthday.html'
     }
 }, 1000);
+
+// music 
+let music=new Audio();
+music.src="audio/bits.mp3"
+music.loop=true;
+music.play();
+
+// mute/unmute  
+let mute=document.getElementById('mute');
+let unmute=document.getElementById('unmute');
+unmute.style.display='none'
+// mute function
+
+mute.addEventListener('click',()=>{
+    music.muted=true;
+    mute.style.display='none';
+    unmute.style.display='block';
+})
+// // unmute function 
+unmute.addEventListener('click',()=>{
+    music.muted=false;
+    mute.style.display='block';
+    unmute.style.display='none';
+})
+
 
 
